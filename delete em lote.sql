c75623d68f275e485eb1a6e60a8013cd4268ddb2
aguiarfpmr <aguiar.felipemr@gmail.com>
2021-10-20 09:50:37 -0300

    Update delete em lote.sql

--- delete em lote.sql
+++ delete em lote.sql
@@ -1,61 +1,27 @@
---use tempdb
---go
-------------------------------------------------------------------------------------------------------------------------------------
----- tabelas teste
-------------------------------------------------------------------------------------------------------------------------------------
---set nocount on
+USE DTS_TOOLS
+GO
 
---drop table if exists tabela6
-
---drop table if exists tabela5
-
---drop table if exists tabela4
-
---drop table if exists tabela3
-
---drop table if exists tabela2
-
---drop table if exists tabela1
-
-
---create table tabela1 (serie varchar(10) --primary key not null, 
---,id_tabela1 int)
-
-----create table tabela2 (id_tabela2 int identity(1,1) primary key not null, serie2 varchar(10) --FILHA da tabela1 
-----constraint 	fk_serie foreign key (serie2) references tabela1 (serie)
-----)
-
-----create table tabela3 (id_tabela3 int identity(1,1) primary key not null, serie varchar(3) DEFAULT NULL, id int, id_tabela2 INT	--FILHA da tabela2 - SEM FK COM A SERIE 
-----constraint 	fk_id_pk foreign key (id_tabela2) references tabela2 (id_tabela2)
-----)
-
-----create table tabela4 (id_tabela4 int, serie varchar(3) , id int, id_tabela3 INT 	 --FILHA da tabela2 - pk composta com a coluna SERIE
-----CONSTRAINT 	PK_COMPOSTA PRIMARY KEY (id_tabela4, serie)
-----constraint 	fk_id_pk2 foreign key (id_tabela3) references tabela3 (id_tabela3)
-----)
-
---CREATE TABLE TABELA5 (ID INT, SERIE VARCHAR(3))
---go
---CREATE TABLE TABELA6 (ID INT)
---go
-
-
----- popula a tabela 1
---set nocount on
---INSERT INTO tabela1 VALUES ( CONVERT(VARCHAR(10),CONVERT(VARCHAR(255),NEWID())),
---							 CONVERT(INT,(RAND()*1000))
---						  )
---GO 10000
-
---IF OBJECT_ID('TEMPDB..tabela1_aux') IS NOT NULL
---DROP TABLE tabela1_aux
---go
---CREATE TABLE tabela1_aux (SERIE VARCHAR(10), ID_TABELA1 INT)
---INSERT INTO tabela1_aux
---SELECT * FROM TABELA1
-
-
---go
+-- Cria Função split string
+create OR ALTER function fSplit (@string varchar(max), @separador char(1)) 
+returns table as return
+    with a as (
+        select
+            id = 1,
+            len_string = len(@string) + 1,
+            ini = 1,
+            fim = coalesce(nullif(charindex(@separador, @string, 1), 0), len(@string) + 1),
+            elemento = ltrim(rtrim(substring(@string, 1, coalesce(nullif(charindex(@separador, @string, 1), 0), len(@string) + 1)-1)))
+        union all
+        select
+            id + 1,
+            len(@string) + 1,
+            convert(int, fim) + 1,
+            coalesce(nullif(charindex(@separador, @string, fim + 1), 0), len_string), 
+            ltrim(rtrim(substring(@string, fim + 1, coalesce(nullif(charindex(@separador, @string, fim + 1), 0), len_string)-fim-1)))
+        from a where fim < len_string)
+    select id, elemento from a 
+    -- incluir with option (maxrecursion 0) na chamada da FC para strings com mais de 100 elementos
+GO
 
 ----------------------------------- começo da proc
 if OBJECT_ID('delete_lote_sp','P') IS NULL
@@ -144,29 +110,12 @@ script_ligacao VARCHAR(8000)
 )
 
 declare @colunas_chave	varchar(150) = ' + '''' + @colunas_chave + '''' + '
-;with CTE as 
-(
-	select
-		id = 1,
-		len_string = len(@colunas_chave) + 1,
-		ini = 1,
-		fim = coalesce(nullif(charindex('','', @colunas_chave, 1), 0), len(@colunas_chave) + 1),
-		elemento = ltrim(rtrim(substring(@colunas_chave, 1, coalesce(nullif(charindex('','', @colunas_chave, 1), 0), len(@colunas_chave) + 1)-1)))
-	UNION ALL
-	select
-		id + 1,
-		len(@colunas_chave) + 1,
-		convert(int, fim) + 1,
-		coalesce(nullif(charindex('','', @colunas_chave, fim + 1), 0), len_string), 
-		ltrim(rtrim(substring(@colunas_chave, fim + 1, coalesce(nullif(charindex('','', @colunas_chave, fim + 1), 0), len_string)-fim-1)))
-	from CTE where fim < len_string
-)
 INSERT INTO ##TEMP_STRING_SPLIT_DELETE_LOTE_SP
 SELECT 
 id, 
 COLUNA = elemento,
 script_ligacao = ''and A.'' + elemento + '' = B.'' + elemento
-FROM CTE
+FROM fSplit (' + '''' + @colunas_chave + '''' + ', '','')
 option (maxrecursion 0)
 '
 

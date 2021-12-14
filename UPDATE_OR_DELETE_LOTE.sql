33a3b83e912d5d52572d459d1f4b8317506684d6
aguiarfpmr <aguiar.felipemr@gmail.com>
2021-12-14 13:42:17 -0300

    Update UPDATE_OR_DELETE_LOTE.sql

--- UPDATE_OR_DELETE_LOTE.sql
+++ UPDATE_OR_DELETE_LOTE.sql
@@ -1,3 +1,25 @@
+-- Cria Função split string
+create OR ALTER function fSplit (@string varchar(max), @separador char(1)) 
+returns table as return
+    with a as (
+        select
+            id = 1,
+            len_string = len(@string) + 1,
+            ini = 1,
+            fim = coalesce(nullif(charindex(@separador, @string, 1), 0), len(@string) + 1),
+            elemento = ltrim(rtrim(substring(@string, 1, coalesce(nullif(charindex(@separador, @string, 1), 0), len(@string) + 1)-1)))
+        union all
+        select
+            id + 1,
+            len(@string) + 1,
+            convert(int, fim) + 1,
+            coalesce(nullif(charindex(@separador, @string, fim + 1), 0), len_string), 
+            ltrim(rtrim(substring(@string, fim + 1, coalesce(nullif(charindex(@separador, @string, fim + 1), 0), len_string)-fim-1)))
+        from a where fim < len_string)
+    select id, elemento from a 
+    -- incluir with option (maxrecursion 0) na chamada da FC para strings com mais de 100 elementos
+GO
+
 
 IF NOT EXISTS (SELECT *
 FROM INFORMATION_SCHEMA.ROUTINES
